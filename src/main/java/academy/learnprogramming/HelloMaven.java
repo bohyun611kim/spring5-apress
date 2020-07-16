d9188f37676a04bfb440d62124162633b0a0a584
tomdanbi <bohyun611.kim@gmail.com>
2020-07-16 12:32:41 +0900

    로깅파일테스트

--- src/main/java/academy/learnprogramming/HelloMaven.java
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,7 +1,14 @@
 package academy.learnprogramming;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloMaven.class);
     public static void main(String[] args) {
         System.out.println("hello maven!");
+        logger.info("Hello Info");
+        logger.debug("Hello Debug");
     }
 }
