99d34f986ccfdb1c8076370ad0bb31bafd1d28cc
tomdanbi <bohyun611.kim@gmail.com>
2020-07-16 16:58:49 +0900

    의존성주입시작

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,31 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
     private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         logger.info("Guess the Number Game!");
+
+        // create context (container)
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        // == 콩을 꺼낸다
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+        // == 램덤 함수 호출 ==
+        int number = numberGenerator.next();
+
+        // log
+        logger.info("number = {}", number);
+
+        // close
+        context.close();
     }
 }
